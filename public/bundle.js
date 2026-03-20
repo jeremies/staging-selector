7c76f4609dbb2236f77984ed046191f073a513b9
Joaquim Monserrat Companys <joaquim.monserrat@i3television.es>
2026-03-20 11:32:29 +0100

    support old browsers

--- public/bundle.js
+++ public/bundle.js
@@ -1,62 +1,85 @@
 // bundle.js
-const IS_TIZEN = Boolean(navigator.userAgent.match(/Tizen/i));
+var IS_TIZEN = /Tizen/i.test(navigator.userAgent);
 
-function loadScript(src) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.onload = resolve;
-    script.onerror = reject;
-    document.head.appendChild(script);
-  });
+function loadScript(src, callback) {
+  var script = document.createElement("script");
+  script.src = src;
+
+  script.onload = function () {
+    callback(null);
+  };
+
+  script.onerror = function () {
+    callback(new Error("Failed to load script: " + src));
+  };
+
+  document.head.appendChild(script);
 }
 
 function injectStyles() {
-  const style = document.createElement("style");
-  style.innerHTML = `
-    :focus {
-      outline: 2px solid red;
-    }
-
-    body {
-      margin: 50px;
-      background-color: white;
-      font-size: 30px;
-    }
-  `;
+  var style = document.createElement("style");
+  style.innerHTML =
+    ":focus { outline: 2px solid red; }" +
+    "body { margin: 50px; background-color: white; font-size: 30px; }";
   document.head.appendChild(style);
 }
 
-async function renderApp() {
-  const app = document.getElementById("root");
+function fetchLinks(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "links.json", true);
 
-  let linksData = [];
-  try {
-    const response = await fetch("links.json");
-    if (!response.ok) throw new Error("Network response was not ok");
-    linksData = await response.json();
-  } catch (error) {
-    console.error("Failed to fetch links.json:", error);
-  }
-
-  app.innerHTML = `
-    <ul>
-      ${linksData.map((link) => `<li><a href="${link.url}">${link.title}</a></li>`).join("\n")}
-    </ul>
-  `;
-
-  const links = app.querySelectorAll("a");
-
-  links.forEach((linkEl) => {
-    linkEl.addEventListener("click", function (e) {
-      if (IS_TIZEN) {
-        e.preventDefault(); // prevent normal navigation
-
-        const link = this.getAttribute("href");
-        window.localStorage.setItem("wrapperUrl", link);
-        document.location.reload();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        try {
+          var data = JSON.parse(xhr.responseText);
+          callback(null, data);
+        } catch (e) {
+          callback(e, []);
+        }
+      } else {
+        callback(new Error("Request failed"), []);
       }
-    });
+    }
+  };
+
+  xhr.send();
+}
+
+function renderApp(callback) {
+  var app = document.getElementById("root");
+
+  fetchLinks(function (err, linksData) {
+    if (err) {
+      console.error("Failed to fetch links.json:", err);
+      linksData = [];
+    }
+
+    var html = "<ul>";
+
+    for (var i = 0; i < linksData.length; i++) {
+      var link = linksData[i];
+      html += '<li><a href="' + link.url + '">' + link.title + "</a></li>";
+    }
+
+    html += "</ul>";
+    app.innerHTML = html;
+
+    var links = app.getElementsByTagName("a");
+
+    for (var j = 0; j < links.length; j++) {
+      links[j].addEventListener("click", function (e) {
+        if (IS_TIZEN) {
+          e.preventDefault();
+
+          var link = this.getAttribute("href");
+          window.localStorage.setItem("wrapperUrl", link);
+          document.location.reload();
+        }
+      });
+    }
+
+    callback();
   });
 }
 
@@ -71,13 +94,19 @@ function initSpatialNavigation() {
   SpatialNavigation.focus();
 }
 
-window.addEventListener("load", async () => {
+window.addEventListener("load", function () {
   injectStyles();
-  await renderApp();
 
-  await loadScript(
-    "https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js",
-  );
-
-  initSpatialNavigation();
+  renderApp(function () {
+    loadScript(
+      "https://luke-chang.github.io/js-spatial-navigation/spatial_navigation.js",
+      function (err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        initSpatialNavigation();
+      },
+    );
+  });
 });
